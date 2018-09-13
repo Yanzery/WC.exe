6bf9c63c9162d4c2bc5aa2ed9d1c8757288c596f
3216005166-郭鹏燕 <1450203917@qq.com>
2018-09-13 17:25:40 +0800

    添加扩展功能

--- wc.cpp
+++ wc.cpp
@@ -2,152 +2,200 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include <io.h>
+#include <iostream>
+using namespace std;
+const int maxlen_filename=80;
+
+struct Twc
+{
+	int character,word,line;
+	int null_line,annotation_line,code_line;
+	Twc() { character=word=line=null_line=annotation_line=code_line=0;}
+}; 
+
+struct Topt
+{
+	bool s,c,w,l,a;
+	Topt() { s=c=w=l=a=false;}
+};
+
+bool is_character(char ch)
+{
+	if (ch>='a' && ch<='z') return true;
+	if (ch>='A' && ch<='Z') return true;
+	if (ch>='0' && ch<='9') return true;
+	return false;
+}
+
+
+
+Twc calc(char* filename)
+{
+	Twc res;
+	FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+   	  printf("打开有误！\n");
+    	printf("请按enter键继续....");
+    	_getch();
+    	exit(0);
+    }
+    char ch,lastch;
+    lastch=0;
+    int char_num=0;
+    int bufsize=105;
+    char st[bufsize];
+    while(!feof(fp))
+    {
+    	fgets(st,bufsize,fp);
+    	res.line++;
+    	int len=strlen(st);    	
+
+    	for (int i=0;i<len;i++)
+    	{
+    		if (is_character(st[i]))
+    		{
+    			res.character++;
+				if  ( i==0 || !is_character(st[i-1]) ) 
+    				res.word++;    		
+    		}
+    	}
+    	
+    	// 代码行与空行的判断 
+    	int show_char=0; 
+    	for (int i=0;i<len;i++)
+    	{
+    		if (st[i]!=' ' && st[i]!='\n' && st[i]!='\t')
+    			show_char++;
+    	}
+    	if (show_char>1) res.code_line++;
+    	else res.null_line++;
+    	
+    	// 注释行的判断
+    	bool is_annotation_line=false;
+    	show_char=0;
+    	for (int i=0;i<len;i++)
+    	{
+    		if ((st[i]!=' ') && st[i]!='\n' && st[i]!='\t')
+    		{
+    			show_char++;
+    			if (st[i]=='/')
+    			{
+    				if (i+1<len && st[i+1]=='/')
+    					is_annotation_line=true;
+    				break;
+    			}
+    			if (show_char>1) break;
+    		}
+    	}
+    	if (is_annotation_line==true) res.annotation_line++;	
+    }
+    
+    return res;
+}
+
+void work(char* filename,Topt now)
+{
+	Twc ans=calc(filename);
+	if (now.c==true)
+	{
+	    printf("The number of characters is %d\n", ans.character);
+	}
+	if (now.w==true)
+	{
+	    printf("The number of words is %d\n", ans.word);
+	}
+	if (now.l==true)
+    {
+	    printf("The number of lines is %d\n", ans.line);
+	}
+	if (now.a==true)
+	{
+	    printf("The number of null line is %d\n", ans.null_line);
+	    printf("The number of annotation line is %d\n", ans.annotation_line);
+	    printf("The number of code line is %d\n", ans.code_line);
+	}
+
+}
+
+
+//递归处理目录下符合条件的文件 
+void listFiles(string dir,Topt nowopt)
+{
+	intptr_t handle;
+	_finddata_t findData;
+ 
+	handle = _findfirst((dir+"\\*.*").c_str(), &findData);    // 查找目录中的第一个文件
+	if (handle == -1)
+	{
+		cout << "Failed to find first file!\n";
+		return;
+	}
+ 
+	do
+	{
+		string nextdir=dir+'/'+string(findData.name);
+		if (strcmp(findData.name, ".") == 0) continue;
+		if (strcmp(findData.name, "..") == 0) continue;
+		if (findData.attrib & _A_SUBDIR )
+		{   
+			    listFiles(nextdir,nowopt);
+		}
+		else 
+		{
+			printf("the file is %s\n",(nextdir).c_str());
+			int len=nextdir.size();
+			char filename[len+1];
+			nextdir.copy(filename, len, 0);//这里5代表复制几个字符，0代表复制的位置，
+			filename[len]='\0';
+			work(filename,nowopt);
+			printf("-------------\n");
+		}
+	} while (_findnext(handle, &findData) == 0);    // 查找目录中的下一个文件
+
+	_findclose(handle);    // 关闭搜索句柄
+
+}
+
 int main(int argc, char * argv[])
 {
-    int wc_char = 0;
-    int wc_word = 1;
-    int wc_line = 1;
-    char filename[80];
+    char filename[maxlen_filename];
     FILE *fp = NULL;
-    if (argc == 1)
-    {
-        printf("missing file name:\nPlease input file name:");
-        gets(filename);
-        fp = fopen(filename, "r");
-        if (fp == NULL)
-        {
-            printf("打开有误！\n");
-            printf("请按enter键继续....");
-            _getch();
-            exit(0);
-            //...
-        }
-        printf("This is the file :\n");
-        while (1)
-        {
-            char ch;
-            ch = fgetc(fp);
-            if (ch >= 'A'&&ch <= 'z')
-            {
-                putchar(ch);
-                wc_char++;
-            }
-            else if (ch == ' ')
-            {
-                putchar(ch);
-                wc_word++;
-            }
-            else if (ch == '\n')
-            {
-                putchar(ch);
-                wc_word++;
-                wc_line++;
-            }
-            else if (ch = EOF)
-            {
-                break;
-            }
-        }
-        printf("The char count is %d\n", wc_char);
-        printf("The word count is %d\n", wc_word);
-        printf("The line count is %d\n", wc_line);
-        fclose(fp);
-        _getch();
-        exit(0);
-    }
-    else if (argc == 2)
-    {
-        fp = fopen(argv[1], "r");
-        if (fp == NULL)
-        {
-            printf("打开有误！\n");
-            printf("请按enter键继续....");
-            _getch();
-            exit(0);
-            //...
-        }
-        while (1)
-        {
-            char ch;
-            ch = fgetc(fp);
-            if (ch >= 'A'&&ch <= 'z')
-            {
-                wc_char++;
-            }
-            else if (ch == ' ')
-            {
-                wc_word++;
-            }
-            else if (ch == '\n')
-            {
-                wc_word++;
-                wc_line++;
-            }
-            else if (ch = EOF)
-            {
-                break;
-            }
-        }
-        printf("文件打成功!\n请输入统计方式如(-c\-w\-l):");
-        gets(filename);
-        if (strcmp(filename, "-c") == 0)
-        {
-            printf("The char count is %d\n", wc_char);
-        }
-        else if (strcmp(filename, "-w") == 0)
-        {
-            printf("The word count is %d\n", wc_word);
-        }
-        else if (strcmp(filename, "-l") == 0)
-        {
-            printf("The line count is %d\n", wc_line);
-        }
-    }
-    else if (argc == 3)
-    {
-        fp = fopen(argv[2], "r");
-        if (fp == NULL)
-        {
-            printf("打开有误！\n");
-            printf("请按enter键继续....");
-            _getch();
-            exit(0);
-            //...
-        }
-        while (1)
-        {
-            char ch;
-            ch = fgetc(fp);
-            if (ch >= 'A'&&ch <= 'z')
-            {
-                wc_char++;
-            }
-            else if (ch == ' ')
-            {
-                wc_word++;
-            }
-            else if (ch == '\n')
-            {
-                wc_word++;
-                wc_line++;
-            }
-            else if (ch = EOF)
-            {
-                break;
-            }
-        }
-        if (strcmp(argv[1],"-c")==0)
-        {
-            printf("The char count is %d\n", wc_char);
-        }
-        else if (strcmp(argv[1],"-w")==0)
-        {
-            printf("The word count is %d\n", wc_word);
-        }
-        else if (strcmp(argv[1],"-l")==0)
-        {
-            printf("The line count is %d\n", wc_line);
-        }
-        fclose(fp);
-    }
+    strcpy(filename,argv[argc-1]);
+    Topt nowopt;
+	for (int i=0;i<argc-1;i++)
+	{
+		if (strcmp(argv[i], "-s") == 0)
+    	{
+        	nowopt.s=true;
+    	}
+    	if (strcmp(argv[i], "-c") == 0)
+    	{
+        	nowopt.c=true;
+    	}
+    	if (strcmp(argv[i], "-w") == 0)
+    	{
+        	nowopt.w=true;
+    	}
+    	if (strcmp(argv[i], "-l") == 0)
+    	{
+        	nowopt.l=true;
+    	}
+    	if (strcmp(argv[i], "-a") == 0)
+    	{
+        	nowopt.a=true;
+    	}
+	}
+	nowopt.s=true;
+	if (nowopt.s==true)
+	{
+		listFiles(string(filename),nowopt);
+	}
+	else
+	{
+		work(filename,nowopt);
+	}
+	
+	return 0;
 }
